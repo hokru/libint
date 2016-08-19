8470348d51a39cb58b4c2d7f8793aa6ae943d1f4
Lori A. Burns <lori.burns@gmail.com>
2016-08-19 11:21:30 -0400

    update cmake_build_type handling

--- cmake/Psi4Macros.cmake
+++ cmake/Psi4Macros.cmake
@@ -8,7 +8,7 @@
 #Syntax: print_option(<option to print> <was specified>)
 #
 macro(print_option variable default)
-if(NOT DEFINED ${variable})
+if(NOT DEFINED ${variable} OR "${${variable}}" STREQUAL "")
     message(STATUS "Setting (unspecified) option ${variable}: ${default}")
 else()
     message(STATUS "Setting option ${variable}: ${${variable}}")
@@ -20,19 +20,22 @@ endmacro()
 #Syntax: option_with_print(<option name> <description> <default value>)
 #
 macro(option_with_print variable msge default)
-print_option(${variable} ${default})
-option(${variable} ${msge} ${default})
+   print_option(${variable} ${default})
+   option(${variable} ${msge} ${default})
 endmacro(option_with_print)
 
 #Wraps an option with a default other than ON/OFF and prints it
 #NOTE: Can't combine with above b/c CMake handles ON/OFF options specially
+#NOTE2: CMAKE_BUILD_TYPE (and other CMake variables) are always defined so need
+#       to further check for if they are the NULL string.  This is also why we
+#       need the force
 #
 #Syntax: option_with_default(<option name> <description> <default value>)
 #
 macro(option_with_default variable msge default)
 print_option(${variable} ${default})
-if(NOT DEFINED ${variable})
-   set(${variable} ${default} CACHE STRING ${msge})
+if(NOT DEFINED ${variable} OR "${${variable}}" STREQUAL "")
+   set(${variable} ${default} CACHE STRING ${msge} FORCE)
 endif()
 endmacro(option_with_default)
 
@@ -82,18 +85,20 @@ endmacro(psi4_add_binary libname sources)
 
 include(CheckCCompilerFlag)
 include(CheckCXXCompilerFlag)
+if(CMAKE_Fortran_COMPILER)
+    include(CheckFortranCompilerFlag)  # CMake >= 3.3, so local copy in cmake/
+endif()
 
-#Checks if C flags are valid, if so adds them to CMAKE_C_FLAGS
-#Input should be a list of flags to try.  If two flags are to be tried together
-#enclose them in quotes, e.g. "-L/path/to/dir -lmylib" is tried as a single
-#flag, whereas "-L/path/to/dir" "-lmylib" is tried as two separate flags.
-#The first list item to succeed is added to CMAKE_C_FLAGS, then try loop
-#breaks. Warning issued if no flags in list suceed.
+#The guts of the next two functions, use the wrappers please
 #
-#Syntax: add_C_flags(<flags to add>)
+#Syntax: add_C_or_CXX_flags(<True for C, False for CXX>)
 #
-macro(add_C_flags)
-   set(CMAKE_REQUIRED_QUIET_SAVE ${CMAKE_REQUIRED_QUIET})
+# Note: resist adding -Werror to the check_X_compiler_flag calls,
+#   as (i) the flag for Intel is actually -diag-error warn, (ii)
+#   Intel ifort doesn't define -Werror, and (iii) passing it
+#   changes REQUIRED_DEFINITIONS.
+macro(add_C_or_CXX_flags is_C)
+set(CMAKE_REQUIRED_QUIET_SAVE ${CMAKE_REQUIRED_QUIET})
    set(CMAKE_REQUIRED_QUIET ON)
    set(flags_to_try "${ARGN}")
    foreach(flag_i IN LISTS flags_to_try ITEMS -brillig)
@@ -102,20 +107,46 @@ macro(add_C_flags)
          break()
       endif()
       unset(test_option CACHE)
-      CHECK_C_COMPILER_FLAG("${flag_i} -Werror" test_option)
-      if(test_option)
-         set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${flag_i}")
-         if(NOT CMAKE_REQUIRED_QUIET_SAVE)
-            message(STATUS "Performing Test CMAKE_C_FLAGS [${flag_i}] - Success, Appending")
-         endif()
-         break()
+      if(${is_C} EQUAL 0)
+          CHECK_C_COMPILER_FLAG("${flag_i}" test_option)
+          set(description_to_print CMAKE_C_FLAGS)
+      elseif(${is_C} EQUAL 1)
+          CHECK_CXX_COMPILER_FLAG("${flag_i}" test_option)
+          set(description_to_print CMAKE_CXX_FLAGS)
+      elseif(${is_C} EQUAL 2)
+          CHECK_Fortran_COMPILER_FLAG("${flag_i}" test_option)
+          set(description_to_print CMAKE_Fortran_FLAGS)
+      endif()
+      set(msg_base "Performing Test ${description_to_print} [${flag_i}] -")
+      if(${test_option})
+        set(${description_to_print} "${${description_to_print}} ${flag_i}")
+        if(NOT CMAKE_REQUIRED_QUIET_SAVE)
+           message(STATUS  "${msg_base} Success, Appending")
+        endif()
+        break()
       else()
-         if(NOT CMAKE_REQUIRED_QUIET_SAVE)
-            message(STATUS "Performing Test CMAKE_C_FLAGS [${flag_i}] - Failed")
-         endif()
+        if(NOT CMAKE_REQUIRED_QUIET_SAVE)
+           message(STATUS "${msg_base} Failed")
+        endif()
       endif()
    endforeach()
-   set(CMAKE_REQUIRED_QUIET ${CMAKE_REQUIRED_QUIET_SAVE})
+   set(CMAKE_REQUIRED_QUIET ${CMAKE_REQUIRED_QUIET_SAVE})  
+endmacro()
+
+
+
+#Checks if C flags are valid, if so adds them to CMAKE_C_FLAGS
+#Input should be a list of flags to try.  If two flags are to be tried together
+#enclose them in quotes, e.g. "-L/path/to/dir -lmylib" is tried as a single
+#flag, whereas "-L/path/to/dir" "-lmylib" is tried as two separate flags.
+#The first list item to succeed is added to CMAKE_C_FLAGS, then try loop
+#breaks. Warning issued if no flags in list succeed.
+#
+#
+#Syntax: add_C_flags(<flags to add>)
+#
+macro(add_C_flags)
+   add_C_or_CXX_flags(0 ${ARGN})
 endmacro()
 
 #Checks if CXX flags are valid, if so adds them to CMAKE_CXX_FLAGS
@@ -124,29 +155,16 @@ endmacro()
 #Syntax: add_CXX_flags(<flags to add>)
 #
 macro(add_CXX_flags)
-   set(CMAKE_REQUIRED_QUIET_SAVE ${CMAKE_REQUIRED_QUIET})
-   set(CMAKE_REQUIRED_QUIET ON)
-   set(flags_to_try "${ARGN}")
-   foreach(flag_i IN LISTS flags_to_try ITEMS -brillig)
-      if(${flag_i} STREQUAL "-brillig")
-         message(WARNING "Option unfulfilled as none of ${flags_to_try} valid")
-         break()
-      endif()
-      unset(test_option CACHE)
-      CHECK_CXX_COMPILER_FLAG("${flag_i} -Werror" test_option)
-      if(test_option)
-         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flag_i}")
-         if(NOT CMAKE_REQUIRED_QUIET_SAVE)
-            message(STATUS "Performing Test CMAKE_CXX_FLAGS [${flag_i}] - Success, Appending")
-         endif()
-         break()
-      else()
-         if(NOT CMAKE_REQUIRED_QUIET_SAVE)
-            message(STATUS "Performing Test CMAKE_CXX_FLAGS [${flag_i}] - Failed")
-         endif()
-      endif()
-   endforeach()
-   set(CMAKE_REQUIRED_QUIET ${CMAKE_REQUIRED_QUIET_SAVE})
+    add_C_or_CXX_flags(1 ${ARGN})
+endmacro()
+
+#Checks if Fortran flags are valid, if so adds them to CMAKE_Fortran_FLAGS
+#See add_C_flags for more info on syntax
+#
+#Syntax: add_Fortran_flags(<flags to add>)
+#
+macro(add_Fortran_flags)
+    add_C_or_CXX_flags(2 ${ARGN})
 endmacro()
 
 #Macro for adding flags common to both C and CXX, if the compiler supports them
@@ -154,8 +172,15 @@ endmacro()
 #Syntax: add_flags(<flags to add>)
 #
 macro(add_flags FLAGS)
-   add_C_flags(${FLAGS})
-   add_CXX_flags(${FLAGS})
+    if(CMAKE_C_COMPILER)
+        add_C_flags(${FLAGS})
+    endif()
+    if(CMAKE_CXX_COMPILER)
+        add_CXX_flags(${FLAGS})
+    endif()
+    if(CMAKE_Fortran_COMPILER)
+        add_Fortran_flags(${FLAGS})
+    endif()
 endmacro()
 
 #Defines an option that if enabled turns on some compiler flags
